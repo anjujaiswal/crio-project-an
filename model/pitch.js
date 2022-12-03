893da8e9b85c53c7975848b48a44d35d8802dd91
anjujaiswal <66971106+anjujaiswal@users.noreply.github.com>
2022-12-04 01:01:30 +0530

    Add files via upload

---
+++ model/pitch.js
@@ -0,0 +1,18 @@
+const mongoose  = require('mongoose')
+const { Schema, model} = mongoose
+
+const pitchSchema = new Schema({
+    id : {type :String  ,required:true},
+    entrepreneur:{type : String, required:true},
+    pitchTitle: {type:String, required:true},
+    pitchIdea: {type:String, required:true},
+    askAmount: {type:Number ,required:true, min:0,max:100},
+    createdAt:Date,
+    offers:[{
+        type:Schema.Types.ObjectId,
+        ref:'Offer'
+    }]
+})
+
+const Pitch = model('Pitch', pitchSchema);
+module.exports = Pitch
